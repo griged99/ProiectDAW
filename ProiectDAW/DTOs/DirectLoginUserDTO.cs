6e24cb6b337a593ee33d8dec4d994cc47c314c2e
eduar <eduar@LAPTOP-E6JL5E90>
2022-06-09 13:00:54 +0200

    Added password encryption and DirectLoginUserDTO

--- ProiectDAW/DTOs/DirectLoginUserDTO.cs
+++ ProiectDAW/DTOs/DirectLoginUserDTO.cs
@@ -1,21 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ProiectDAW.DTOs
 {
     public class DirectLoginUserDTO
     {
         [Required]
-        public string FirstName { get; set; }
-
-        [Required]
-        public string LastName { get; set; }
+        [MinLength(6)]
+        public string Password { get; set; }
 
         [Required]
         [EmailAddress]
         public string Email { get; set; }
-
-        [Required]
-        [MinLength(6)]
-        public string Password { get; set; }
     }
 }
